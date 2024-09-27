45190bbd7bebdbc80e11d5059aa6c95794b230e7
Sudhir <sudhir1072004@gmail.com>
2024-09-27 11:16:46 +0530

    Created Route for login chat profileupdate. Created Login form with usestate , added css to it .Added Poppin font in index.css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import assets from '../../assets/assets'
 
 const Login = () => {
+
+    const [currState,setCurrState]=useState('Sign up');
+
   return (
-    <div>Login</div>
+    <div className='login'>
+        <img src={assets.logo_big} alt="" className="logo" />
+        <form className="login-form">
+            <h2>{currState}</h2>
+            {currState === "Sign up" ? <input type="text" placeholder="Username" className="form-input" required/> : null}
+            <input type="email" placeholder='Email' className="form-input" required />
+            <input type="password" placeholder='Password' className="form-input" required/>
+            <button type='submit'>{currState === 'Sign up' ? 'Create account' : 'Login'}</button>
+            <div className="login-term">
+                <input type="checkbox" />
+                <p>Agree to the terms of use & privacy policy.</p>
+            </div>
+            <div className="login-forgot">
+                {
+                    currState === 'Sign up' ? 
+                        <p className='login-toggle'>Already have an account <span onClick={()=>setCurrState('Login')}>Login here</span></p> :
+                        <p className='login-toggle'>Create an account <span onClick={()=>setCurrState('Sign up')}>Click here</span></p>
+                }
+            </div>
+        </form>
+    </div>
   )
 }
 
