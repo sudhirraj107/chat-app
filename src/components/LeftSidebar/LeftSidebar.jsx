46f2e06e77981c4bb823d08fa0bb993f78505b0f
Sudhir <sudhir1072004@gmail.com>
2024-09-29 22:08:44 +0530

    Created Upload js,AppContext . Added logic to ProfileUpdate

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './LeftSidebar.css'
 import assets from '../../assets/assets.js'
+import { useNavigate } from 'react-router-dom'
+import { collection, getDoc, getDocs, query, where } from 'firebase/firestore'
+import { db } from '../../config/firebase.js'
+import { AppContext } from '../../context/AppContext.jsx'
 
 const LeftSidebar = () => {
+
+    const navigate=useNavigate();
+    const {userData} = useContext(AppContext)
+    const [user,setUser] = useState(null);
+    const [showSearch,setShowSearch] = useState(false)
+
+    const inputHandler = async (e)=> {
+        try {
+            const input = e.target.value;
+            if (input) {
+                setShowSearch(true);
+                const useRef = collection(db,'users');
+                const q = query(useRef,where('username','==',input.toLowerCase()))
+                const querySnap = await getDocs(q);
+                if(!querySnap.empty && querySnap.docs[0].data().id !== userData.id){
+                    setUser(querySnap.docs[0].data());
+                }
+                else{
+                    setUser(null)
+                }
+            }
+            else{
+                setShowSearch(false)
+            }
+        } catch (error) {
+            
+        }
+    }
+
   return (
     <div className='ls'>
         <div className="ls-top">
@@ -11,7 +44,7 @@ const LeftSidebar = () => {
                 <div className="menu">
                     <img src={assets.menu_icon} alt="" />
                     <div className="sub-menu">
-                        <p>Edit Profile</p>
+                        <p onClick={()=> navigate('/profile')}>Edit Profile</p>
                         <hr />
                         <p>Logout</p>
                     </div>
@@ -19,7 +52,7 @@ const LeftSidebar = () => {
             </div>
             <div className="ls-search">
                 <img src={assets.search_icon} alt="" />
-                <input type="text" placeholder='Search here'/>
+                <input onChange={inputHandler} type="text" placeholder='Search here'/>
             </div>
         </div>
         <div className="ls-list">
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,80 @@
+import { createContext, useEffect, useState } from "react";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../config/firebase";
+
+
+export const AppContext = createContext();
+
+const AppContextProvider = (props) => {
+
+    const navigate = useNavigate();
+
+    const[userData,setUserData] = useState(null);
+    const[chatData,setChatData] = useState(null);
+
+    const loadUserData = async (uid) => {
+        try {
+            const userRef = doc(db,'users',uid)
+            const userSnap = await getDoc(userRef);
+            const userData = userSnap.data();
+            setUserData(userData)
+            if(userData.avatar && userData.name){
+                navigate('/chat')
+            }
+            else{
+                navigate('/profile')
+            }
+            await updateDoc(userRef,{
+                lastSeen:Date.now()
+            })
+            setInterval(async()=>{ //await can be added only within async
+                if(auth.chatUser){
+                    await updateDoc(userRef,{
+                        lastSeen:Date.now()
+                    })
+                }
+            }, 60000);
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect(()=>{
+        if(userData){
+            const chatRef = doc(db,'chats',userData.id);
+            const unSub = onSnapshot(chatRef, async(res) => {
+                const chatItems = res.data().chatsData;
+                const tempData = [];
+                for(const item of chatItems){
+                    const userRef = doc(db,'users',item.rId);
+                    const userSnap = await getDoc(userRef);
+                    const userData = userSnap.data();
+                    tempData.push(...item,userData)
+                }
+                setChatData(tempData.sort((a,b)=>b.updatedAt - a.updatedAt))
+
+            })
+            return () => {
+                unSub();
+            }
+        }
+    },[userData])
+
+    const value = {
+        userData,setUserData,
+        chatData,setChatData,
+        loadUserData
+    }
+
+    return (
+        <>
+            <AppContext.Provider value={value}>
+                {props.children}
+            </AppContext.Provider>
+        </>
+    )
+}
+
+export default AppContextProvider
